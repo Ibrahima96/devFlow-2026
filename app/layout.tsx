9cdddf107b2291a4d60838cdc524159e5cb0c06c
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2026-02-26 10:25:33 +0000

    init

---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import { Inter, Space_Grotesk } from 'next/font/google';
+import "./globals.css";
+import React from "react";
+import ThemeProvider from "@/context/Theme";
+import Navbar from "@/components/navigation/navbar";
+
+
+
+const inter = Inter({
+    variable: "--font-Inter",
+    subsets: ["latin"],
+});
+const spaceGrotesk = Space_Grotesk({
+    variable: "--font-space-grotesk",
+    subsets: ["latin"],
+});
+
+
+export const metadata: Metadata = {
+    title: "DevFlow",
+    description:
+        "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+    icons: "/images/site-logo.svg",
+
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body
+                className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}
+            >
+                <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true} disableTransitionOnChange={true}>
+                    <Navbar />
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    );
+}
