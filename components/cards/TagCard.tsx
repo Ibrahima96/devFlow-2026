04d3e7365936d31f84e85898ce5396f8c32dd834
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2026-03-02 23:49:12 +0000

    feat: Add utility functions for Devicon class names and tech descriptions, integrate in TagCard component

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from "next/link";
 import ROUTES from "@/constants/route";
 import {Badge} from "@/components/ui/badge";
+import {getDeviconClassName, getTechDescription} from "@/lib/utils";
 
 interface Props {
     _id: string;
@@ -12,11 +13,13 @@ interface Props {
 }
 
 const TagCard = ({_id, name, questions, showCount, compact}: Props) => {
+    const iconClass = getDeviconClassName(name);
+    const iconDescription = getTechDescription(name);
     return (
         <Link href={ROUTES.TAGS(_id)} className={"flex justify-between gap-3"}>
             <Badge
                 className={"background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase"}>
-                <i>icone</i>
+                <i className={`${iconClass} text-sm`}></i>
                 <span> {name}</span>
             </Badge>
             {showCount && (<p className={"small-medium text-dark500_light700"}>{questions}</p>)}
