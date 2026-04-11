ba7180522fbacd5d5a8d7680d78d32db674dda3e
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2026-04-11 15:50:43 +0000

    Immplement input Tags

--- constants/route.ts
+++ constants/route.ts
@@ -5,7 +5,7 @@ const ROUTES = {
     ASK_QUESTION: "/ask-question",
     QUESTION: (id: string) => `/question/${id}`,
     PROFILE: (id: string) => `/profile/${id}`,
-    TAGS: (id: string) => `/tags/${id}`
+    TAG: (id: string) => `/tags/${id}`
 }
 
 export default ROUTES
