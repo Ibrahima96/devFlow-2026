d3ee02c4959e244c34b92e51ca6a49c512d9ff47
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2026-04-11 18:15:07 +0000

    Create User model

---
+++ database/user.model.ts
@@ -0,0 +1,28 @@
+import { model, models, Schema } from "mongoose";
+
+interface IUser {
+    name: string;
+    username: string;
+    email: string;
+    password: string;
+    image: string;
+    bio: string;
+    location: string;
+    porfolio: string;
+    reputation: number;
+}
+const UserSchema = new Schema<IUser>({
+    name: { type: String, require: true },
+    username: { type: String, require: true },
+    email: { type: String, require: true, unique: true },
+    password: { type: String, require: true },
+    image: { type: String, require: true },
+    bio: { type: String },
+    location: { type: String },
+    porfolio: { type: String },
+    reputation: { type: Number, default: 0 }
+}, { timestamps: true })
+
+const User = models?.user || model<IUser>("User", UserSchema);
+
+export default User;
