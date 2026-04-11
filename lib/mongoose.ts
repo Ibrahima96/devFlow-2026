3980e521a76ad449e185ce23a661013034c0fd0f
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2026-04-11 17:37:41 +0000

    setup mongoose and mongodb

---
+++ lib/mongoose.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+    throw new Error("Please add your Mongo URI to .env.local");
+}
+
+let cached = (global as any).mongoose;
+
+if (!cached) {
+    cached = (global as any).mongoose = { conn: null };
+}
+
+const dbConnect = async () => {
+    if (cached.conn) return cached.conn;
+
+    cached.conn = await mongoose.connect(MONGODB_URI, {
+        dbName: "devflow",
+    });
+    return cached.conn;
+};
+
+export default dbConnect;
